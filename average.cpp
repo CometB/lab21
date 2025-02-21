75ad280605c2c6f93f2520fb7f960f2bcfd2ae80
CometB <126880018+CometB@users.noreply.github.com>
2025-02-21 14:56:51 +0700

    .

--- average.cpp
+++ average.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+    double sum = 0;
+
+    for(int i=1;i<=(argc-1);i++){
+        sum += atof(argv[i]);
+    }
+
+    if(argc > 1){
+        double average = sum/(argc-1);
+        cout << "---------------------------------\n";
+        cout << "Average of " << argc-1 << " numbers = "  << average << '\n';
+        cout << "---------------------------------\n";
+    } else{
+        cout << "Please input numbers to find average.";
+    }
+
+    return 0;
+}
